f6ce198fb3990e2ca971a7b24b9cfa47fb51167a
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2023-12-18 12:38:27 -0300

    Segundo Commit: Categorias

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,10 @@ const express = require ('express'),
 router = express.Router(),
 userRouter = require('./User.router')
 // postRouter = require('./Post.router'),
-// categoryRouters = require('./Category.router')
+categoryRouters = require('./Category.router')
 
 router.use('/user', userRouter)
 // router.use('/post', postRouter)
-// router.use('/category', categoryRouters)
+router.use('/category', categoryRouters)
 
 module.exports = router
