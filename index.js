e48d33955212843ed23a2898c8e41a5335fb4f2a
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2023-12-19 16:16:33 -0300

    Configuración MercadoPago

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 require('dotenv').config()
+const mercadopago = require("mercadopago")
+const { update } = require('./src/models/Product.model')
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./src/routes/index')
 const app = express()
 const cors = require('cors')
+
+mercadopago.configure({
+    access_token: "TEST-8003479140000251-121721-b666f0691b81ac45462c970ac9b315fd-1596855217"
+})
+
+app.post("/mercadopago", async (req, res) => {
+    const preference = req.body
+    const responseMP = await mercadopago.preferences.create(preference)
+    console.log(responseMP)
+    res.json({
+        checkoutId: responseMP.body.id
+    });
+
+})
 
 const corsOptions = {
     origin: '*',
@@ -16,10 +32,9 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 mongoose.set("strictQuery", false);
-// mongoose.connect(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI, () => {
     console.log("Connected to MongoDB");
-  });
+});
 
 app.use('/v1', routes)
 
